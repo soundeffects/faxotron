83734641fe7f353cbdeb5d6db87e4958b6448d0d
Samuel Youngblood <sam@Upgraded-iMac.local>
2018-07-31 16:39:50 -0600

    Filling out fax form

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 import Header from './header';
+import FaxForm from './faxForm';
 
 class App extends Component {
   render() {
-    return [<Header/>];
+    return [<Header/>, <FaxForm/>];
   }
 }
 
--- src/components/faxForm.js
+++ src/components/faxForm.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
+import { TextField } from '@material-ui/core';
+
+const styles = {
+    section: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    textField: {
+        width: '20em'
+    },
+    press: {
+        alignItems: 'center',
+        justifyContent: 'end'
+    }
+}
 
 class FaxForm extends Component {
-  
+  render() {
+    return <section style={styles.section}>
+      <TextField style={styles.textField} label='Email'/>
+      <TextField style={styles.textField} label='Phone Number'/>
+      <button style={styles.press}/>
+    </section>
+  };
 }
+
+export default FaxForm;
